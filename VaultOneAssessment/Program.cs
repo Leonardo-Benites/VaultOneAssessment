4ede16d1227946a4f3f761040711fb0721d019c6
LeoBenites <leonardo.benites@performait.com>
2025-01-27 19:01:56 -0300

    feat: Adicionando configurações de inicialização, jwt e connectionstring

--- VaultOneAssessment/Program.cs
+++ VaultOneAssessment/Program.cs
@@ -1,25 +1,107 @@
+using Application.Services;
+using Application.Interfaces;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Precisa ter a senha do sql server definida nas variáveis de ambiente 
+var password = Environment.GetEnvironmentVariable("SQLSERVER_PASSWORD");
+string sqlConnection = builder.Configuration.GetConnectionString("SqlServerConnection");
+
+string connectionString = "";
+
+if (!string.IsNullOrEmpty(password))
+{
+    connectionString = sqlConnection + password + ";";
+}
+else
+{
+    connectionString = sqlConnection;
+}
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+
+//Precisa ter a chave de autenticação definida nas variáveis de ambiente 
+var authenticationKey = Environment.GetEnvironmentVariable("AUTH_KEY");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "Bearer";
+    options.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+    };
+
+    if (!string.IsNullOrEmpty(authenticationKey))
+    {
+        options.TokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationKey));
+    }
+});
+
+// Register services
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddTransient<IAuthenticationService, AuthenticationService>();
+
+builder.Services.AddAutoMapper(typeof(Program));  // AutoMapper configuration
+builder.Services.AddAutoMapper(typeof(MappingProfileService));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost",
+        policy => policy.WithOrigins("http://localhost:8080")  // Frontend URL
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+});
+
+// Add Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        options.RoutePrefix = string.Empty; 
+    });
+}
+else
+{
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+app.UseCors("AllowLocalhost"); 
 
+app.UseRouting();
+app.UseStaticFiles();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
